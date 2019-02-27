79d7f55ee8889d645a7cba9abb7d7ddfef66c1af
Pi-Yueh Chuang <pychuang@gwu.edu>
2019-02-27 13:46:23 -0500

    modify run, createtopo, and createhydro to accomodate re-organizing

--- utilities/run.py
+++ utilities/run.py
@@ -11,9 +11,6 @@ Run xgeoclaw solver with a given case.
 """
 import os
 import sys
-from clawpack.clawutil import runclaw
-import createtopo
-import createhydro
 
 
 def create_data(casepath):
@@ -22,6 +19,8 @@ def create_data(casepath):
     Args:
         casepath [in]: a string indicating the abs path of case folder.
     """
+    import createtopo
+    import createhydro
 
     if not os.path.isdir(casepath):
         print("Error: case folder {} does not exist.".format(casepath),
@@ -34,26 +33,20 @@ def create_data(casepath):
               file=sys.stderr)
         sys.exit(1)
 
-
-    pwd = os.getcwd() # get current working directory
-
     sys.path.insert(0, casepath) # add case folder to module search path
     import setrun # import the setrun.py
 
+    pwd = os.getcwd() # get current working directory
     os.chdir(casepath) # go to case folder
-
     rundata = setrun.setrun() # get ClawRunData object
+    rundata.write() # write *.data to the case folder
+    os.chdir(pwd) # go back to pwd
 
     # check if topo file exists. Download it if not exist
     createtopo.check_download_topo(casepath, rundata)
 
     # check if hudro file exists. Download it if not exist
     createhydro.check_download_hydro(casepath, rundata)
-
-    rundata.write() # write *.data to the case folder
-
-    os.chdir(pwd) # go back to pwd
-
 
 if __name__ == "__main__":
 
@@ -63,18 +56,24 @@ if __name__ == "__main__":
         print("\tpython run.py case_folder_name")
         sys.exit(0)
 
-    # get case path
-    casepath = os.path.abspath(sys.argv[1])
+    # paths
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    claw_dir = os.path.join(repo_dir, "solver", "clawpack")
+    case_dir = os.path.abspath(sys.argv[1])
+    solver = os.path.join(repo_dir, "solver", "bin", "xgeoclaw")
 
-    # get the abs path of the repo
-    repopath = os.path.dirname(os.path.abspath(__file__))
+    # set CLAW environment variable to satisfy some Clawpack functions' need
+    os.environ["CLAW"] = claw_dir
+
+    # make clawpack searchable
+    sys.path.insert(0, claw_dir)
+    from clawpack.clawutil import runclaw
 
     # create *.data files
-    create_data(casepath)
+    create_data(case_dir)
 
     # run simulation
     runclaw.runclaw(
-        xclawcmd=os.path.join(repopath, "bin/xgeoclaw"),
-        outdir=os.path.join(casepath, "_output"),
-        overwrite=False, restart=None, rundir=casepath, print_git_status=True,
+        xclawcmd=solver, outdir=os.path.join(case_dir, "_output"),
+        overwrite=False, restart=None, rundir=case_dir, print_git_status=True,
         nohup=False, nice=None)
