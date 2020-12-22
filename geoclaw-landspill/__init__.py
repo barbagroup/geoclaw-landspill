43f20836c4cfceff9b2aeeacb6de892af913577c
Pi-Yueh Chuang <pychuang@gwu.edu>
2020-12-22 17:45:30 -0500

    restructure the files for preparation for pip packaging

---
+++ geoclaw-landspill/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Pi-Yueh Chuang <pychuang@pm.me>
+#
+# Distributed under terms of the BSD 3-Clause license.
+
+"""__init__.py"""
--- geoclaw-landspill/run.py
+++ geoclaw-landspill/run.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-########################################################################################################################
+
+####################################################################################################
 # Copyright © 2019-2020 Pi-Yueh Chuang and Lorena A. Barba.
 # All Rights Reserved.
 #
@@ -13,29 +14,31 @@
 #
 # BSD-3-Clause License:
 #
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided
-# that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
 #
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
-#    following disclaimer.
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+#    and the following disclaimer.
 #
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-#    following disclaimer in the documentation and/or other materials provided with the distribution.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+#    conditions and the following disclaimer in the documentation and/or other materials provided
+#    with the distribution.
 #
-# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
-#    promote products derived from this software without specific prior written permission.
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+#    endorse or promote products derived from this software without specific prior written
+#    permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-########################################################################################################################
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+# WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+####################################################################################################
 
-"""
-Run xgeoclaw solver with a given case.
+""" Execute a landspill simulation.
 """
 import os
 import sys
@@ -61,20 +64,21 @@ def create_data(casepath):
               file=sys.stderr)
         sys.exit(1)
 
-    sys.path.insert(0, casepath) # add case folder to module search path
-    import setrun # import the setrun.py
+    sys.path.insert(0, casepath)  # add case folder to module search path
+    import setrun  # import the setrun.py
 
-    pwd = os.getcwd() # get current working directory
-    os.chdir(casepath) # go to case folder
-    rundata = setrun.setrun() # get ClawRunData object
-    rundata.write() # write *.data to the case folder
-    os.chdir(pwd) # go back to pwd
+    pwd = os.getcwd()  # get current working directory
+    os.chdir(casepath)  # go to case folder
+    rundata = setrun.setrun()  # get ClawRunData object
+    rundata.write()  # write *.data to the case folder
+    os.chdir(pwd)  # go back to pwd
 
     # check if topo file exists. Download it if not exist
     createtopo.check_download_topo(casepath, rundata)
 
     # check if hudro file exists. Download it if not exist
     createhydro.check_download_hydro(casepath, rundata)
+
 
 if __name__ == "__main__":
 
