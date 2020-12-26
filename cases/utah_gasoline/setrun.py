b5c106567d731df08be2ee95343aef72c31d2f5e
Pi-Yueh Chuang <pychuang@gwu.edu>
2020-12-26 12:15:02 -0500

    modify setrun.py in all cases to fit new design

--- cases/utah_gasoline/setrun.py
+++ cases/utah_gasoline/setrun.py
@@ -1,284 +1,178 @@
-"""
-Module to set up run time parameters for Clawpack.
+"""Module to set up run time parameters for Clawpack.
 
 The values set in the function setrun are then written out to data files
 that will be read in by the Fortran code.
 
 """
+# pylint: disable=no-member, too-many-statements
+import functools
+from gclandspill.clawutil import data
+from gclandspill.data import LandSpillData
 
-from __future__ import absolute_import
-from __future__ import print_function
-import os
-import numpy
 
+def setrun(claw_pkg: str = 'geoclaw'):
+    """Define the parameters used for running Clawpack.
 
-#------------------------------
-def setrun(claw_pkg='geoclaw'):
-#------------------------------
+    Arguments
+    ---------
+    claw_pkg : str
+        Expected to be "geoclaw" for this setrun.
 
+    Returns
+    -------
+    rundata : clawutil.ClawRunData
     """
-    Define the parameters used for running Clawpack.
-
-    INPUT:
-        claw_pkg expected to be "geoclaw" for this setrun.
-
-    OUTPUT:
-        rundata - object of class ClawRunData
-
-    """
-
-    from clawpack.clawutil import data
 
     assert claw_pkg.lower() == 'geoclaw',  "Expected claw_pkg = 'geoclaw'"
 
     num_dim = 2
     rundata = data.ClawRunData(claw_pkg, num_dim)
 
-    #------------------------------------------------------------------
     # GeoClaw specific parameters:
-    #------------------------------------------------------------------
     rundata = setgeo(rundata)
 
-    #------------------------------------------------------------------
     # AmrClaw specific parameters:
-    #------------------------------------------------------------------
     rundata = setamr(rundata)
 
-    #------------------------------------------------------------------
-    # Standard Clawpack parameters to be written to claw.data:
-    #   (or to amr2ez.data for AMR)
-    #------------------------------------------------------------------
-    clawdata = rundata.clawdata  # initialized when rundata instantiated
+    # an alias to object holding standard Clawpack parameters
+    clawdata = rundata.clawdata
 
-
-    # Set single grid parameters first.
-    # See below for AMR parameters.
-
-
-    # ---------------
-    # Spatial domain:
-    # ---------------
-
-    # Number of space dimensions:
+    # number of space dimensions:
     clawdata.num_dim = num_dim
 
-    # Lower and upper edge of computational domain:
+    # lower and upper edge of computational domain:
     clawdata.lower[0] = -12459650.-400.
     clawdata.upper[0] = -12459650.+400.
-
     clawdata.lower[1] = 4986000.-400
     clawdata.upper[1] = 4986000.+400
 
-
-
-    # Number of grid cells: Coarsest grid
+    # number of grid cells: coarsest grid
     clawdata.num_cells[0] = 200
     clawdata.num_cells[1] = 200
 
-    # ---------------
-    # Size of system:
-    # ---------------
-
-    # Number of equations in the system:
+    # number of equations in the system: continuity eq. and x, y momemtums
     clawdata.num_eqn = 3
 
-    # Number of auxiliary variables in the aux array (initialized in setaux)
+    # number of auxiliary variables: elevation and hydrological data
     clawdata.num_aux = 2
 
-    # Index of aux array corresponding to capacity function, if there is one:
+    # index of aux array corresponding to capacity function, if there is one:
     clawdata.capa_index = 0
 
-
-
-    # -------------
-    # Initial time:
-    # -------------
-
+    # initial time:
     clawdata.t0 = 0.0
 
-
-    # Restart from checkpoint file of a previous run?
-    # If restarting, t0 above should be from original run, and the
-    # restart_file 'fort.chkNNNNN' specified below should be in
-    # the OUTDIR indicated in Makefile.
-
+    # restart from checkpoint file of a previous run?
     clawdata.restart = False               # True to restart from prior results
     clawdata.restart_file = 'fort.chk00006'  # File to use for restart data
 
-    # -------------
-    # Output times:
-    #--------------
-
-    # Specify at what times the results should be written to fort.q files.
-    # Note that the time integration stops after the final output time.
-    # The solution at initial time t0 is always written in addition.
-
+    # temporal data output style
     clawdata.output_style = 1
 
-    if clawdata.output_style == 1:
-        # Output nout frames at equally spaced times up to tfinal:
+    if clawdata.output_style == 1:  # equally spaced times up to tfinal
         clawdata.num_output_times = 240
         clawdata.tfinal = 28800
         clawdata.output_t0 = True  # output at initial (or restart) time?
-
-    elif clawdata.output_style == 2:
-        # Specify a list of output times.
+    elif clawdata.output_style == 2:  # a list of output times
         clawdata.output_times = [0., 1800.]
-
-    elif clawdata.output_style == 3:
-        # Output every iout timesteps with a total of ntot time steps:
+    elif clawdata.output_style == 3:  # every iout timesteps with a total of ntot time steps:
         clawdata.output_step_interval = 80
         clawdata.total_steps = 4000
         clawdata.output_t0 = True
 
+    # output data format
+    clawdata.output_format = 'binary'
 
-    clawdata.output_format = 'binary'      # 'ascii' or 'netcdf'
+    # which veriable to output
+    clawdata.output_q_components = 'all'  # could be list such as [True,True]
 
-    clawdata.output_q_components = 'all'   # could be list such as [True,True]
+    # which aux data to output
     clawdata.output_aux_components = 'all'  # could be list
-    clawdata.output_aux_onlyonce = False    # output aux arrays only at t0
 
+    # whether to output aux data only at t0
+    clawdata.output_aux_onlyonce = False
 
-
-    # ---------------------------------------------------
-    # Verbosity of messages to screen during integration:
-    # ---------------------------------------------------
-
-    # The current t, dt, and cfl will be printed every time step
-    # at AMR levels <= verbosity.  Set verbosity = 0 for no printing.
-    #   (E.g. verbosity == 2 means print only on levels 1 and 2.)
+    # print information up to this AMR level
     clawdata.verbosity = 3
 
-
-
-    # --------------
-    # Time stepping:
-    # --------------
-
-    # if dt_variable==1: variable time steps used based on cfl_desired,
-    # if dt_variable==0: fixed time steps dt = dt_initial will always be used.
+    # use veriable time step size based on CFL
     clawdata.dt_variable = 1
 
-    # Initial time step for variable dt.
-    # If dt_variable==0 then dt=dt_initial for all steps:
-    dx = (clawdata.upper[0] - clawdata.lower[0]) / clawdata.num_cells[0]
-    dy = (clawdata.upper[1] - clawdata.lower[1]) / clawdata.num_cells[1]
-    dx /= numpy.prod(rundata.amrdata.refinement_ratios_x[:rundata.amrdata.amr_levels_max])
-    dy /= numpy.prod(rundata.amrdata.refinement_ratios_y[:rundata.amrdata.amr_levels_max])
+    # data required to calculate a proper initial time step size
+    cell_area = \
+        (clawdata.upper[0] - clawdata.lower[0]) * (clawdata.upper[1] - clawdata.lower[1]) / \
+        (clawdata.num_cells[0] * clawdata.num_cells[1])
+    cell_area /= functools.reduce(
+        lambda x, y: x*y, rundata.amrdata.refinement_ratios_x[:rundata.amrdata.amr_levels_max], 1)
+    cell_area /= functools.reduce(
+        lambda x, y: x*y, rundata.amrdata.refinement_ratios_y[:rundata.amrdata.amr_levels_max], 1)
     vrate = rundata.landspill_data.point_sources.point_sources[0][-1][0]
 
-    clawdata.dt_initial = 0.3 * dx * dy / vrate
+    # initial time step for variable dt.
+    clawdata.dt_initial = 0.3 * cell_area / vrate
 
-    # Max time step to be allowed if variable dt used:
+    # max time step allowed if variable dt
     clawdata.dt_max = 4.0
 
-    # Desired Courant number if variable dt used, and max to allow without
-    # retaking step with a smaller dt:
+    # desired CFL if variable dt, and max CFL allowed without retaking a step with a smaller dt
     clawdata.cfl_desired = 0.9
     clawdata.cfl_max = 0.95
 
-    # Maximum number of time steps to allow between output times:
+    # maximum number of time steps allowed between two output times:
     clawdata.steps_max = 100000
 
-
-
-
-    # ------------------
-    # Method to be used:
-    # ------------------
-
-    # Order of accuracy:  1 => Godunov,  2 => Lax-Wendroff plus limiters
+    # order of accuracy:  1 => Godunov,  2 => Lax-Wendroff plus limiters
     clawdata.order = 2
 
-    # Use dimensional splitting? (not yet available for AMR)
+    # use dimensional splitting? (not yet available for AMR)
     clawdata.dimensional_split = 'unsplit'
 
-    # For unsplit method, transverse_waves can be
-    #  0 or 'none'      ==> donor cell (only normal solver used)
-    #  1 or 'increment' ==> corner transport of waves
-    #  2 or 'all'       ==> corner transport of 2nd order corrections too
+    # 2 or 'all' => corner transport of 2nd order corrections too
     clawdata.transverse_waves = 2
 
-    # Number of waves in the Riemann solution:
+    # number of waves in the Riemann solution:
     clawdata.num_waves = 3
 
-    # List of limiters to use for each wave family:
-    # Required:  len(limiter) == num_waves
-    # Some options:
-    #   0 or 'none'     ==> no limiter (Lax-Wendroff)
-    #   1 or 'minmod'   ==> minmod
-    #   2 or 'superbee' ==> superbee
-    #   3 or 'mc'       ==> MC limiter
-    #   4 or 'vanleer'  ==> van Leer
+    # list of limiters to use for each wave family:
     clawdata.limiter = ['mc', 'mc', 'mc']
 
-    clawdata.use_fwaves = True    # True ==> use f-wave version of algorithms
+    # use f-wave version of algorithms
+    clawdata.use_fwaves = True
 
-    # Source terms splitting:
-    #   src_split == 0 or 'none'    ==> no source term (src routine never called)
-    #   src_split == 1 or 'godunov' ==> Godunov (1st order) splitting used,
-    #   src_split == 2 or 'strang'  ==> Strang (2nd order) splitting used,  not recommended.
+    # src_split == 1 or 'godunov' ==> Godunov (1st order) splitting used,
     clawdata.source_split = 'godunov'
 
-
-    # --------------------
-    # Boundary conditions:
-    # --------------------
-
-    # Number of ghost cells (usually 2)
+    # number of ghost cells (usually 2)
     clawdata.num_ghost = 2
 
-    # Choice of BCs at xlower and xupper:
-    #   0 => user specified (must modify bcN.f to use this option)
-    #   1 => extrapolation (non-reflecting outflow)
-    #   2 => periodic (must specify this at both boundaries)
-    #   3 => solid wall for systems where q(2) is normal velocity
-
+    # BC: 1 => extrapolation (non-reflecting outflow)
     clawdata.bc_lower[0] = 1
     clawdata.bc_upper[0] = 1
-
     clawdata.bc_lower[1] = 1
     clawdata.bc_upper[1] = 1
 
-    # Specify when checkpoint files should be created that can be
-    # used to restart a computation.
-
+    # checkout mechanism selection
     clawdata.checkpt_style = 0
 
-    if clawdata.checkpt_style == 0:
-        # Do not checkpoint at all
+    if clawdata.checkpt_style == 0:  # do not checkpoint at all
         pass
-
-    elif clawdata.checkpt_style == 1:
-        # Checkpoint only at tfinal.
+    elif clawdata.checkpt_style == 1:  # checkpoint only at tfinal.
         pass
-
-    elif clawdata.checkpt_style == 2:
-        # Specify a list of checkpoint times.
-        clawdata.checkpt_times = [0.1,0.15]
-
-    elif clawdata.checkpt_style == 3:
-        # Checkpoint every checkpt_interval timesteps (on Level 1)
-        # and at the final time.
+    elif clawdata.checkpt_style == 2:  # specify a list of checkpoint times.
+        clawdata.checkpt_times = [0.1, 0.15]
+    elif clawdata.checkpt_style == 3:  # checkpoint every checkpt_interval timesteps (on Level 1)
         clawdata.checkpt_interval = 5
 
     return rundata
-    # end of function setrun
-    # ----------------------
 
 
-#------------------------------
-def setamr(rundata):
-#------------------------------
-    """
-    Set AmrClaw specific runtime parameters
-    """
+def setamr(rundata: data.ClawRunData):
+    """Set AmrClaw specific runtime parameters."""
 
     try:
         amrdata = rundata.amrdata
-    except:
-        print("*** Error, this rundata has no amrdata attribute")
-        raise AttributeError("Missing amrdata attribute")
+    except AttributeError as err:
+        raise AttributeError("This rundata has no amrdata attribute") from err
 
     # max number of refinement levels:
     amrdata.amr_levels_max = 2
@@ -288,32 +182,24 @@ def setamr(rundata):
     amrdata.refinement_ratios_y = [4]
     amrdata.refinement_ratios_t = [4]
 
-
-    # Specify type of each aux variable in amrdata.auxtype.
-    # This must be a list of length maux, each element of which is one of:
-    #   'center',  'capacity', 'xleft', or 'yleft'  (see documentation).
-
+    # aux variables are defined at cell centers
     amrdata.aux_type = ['center', 'center']
 
-
-    # Flag using refinement routine flag2refine rather than richardson error
-    amrdata.flag_richardson = False    # use Richardson?
+    # use flag2refine routine to flag refinement (rather than using richardson error)
+    amrdata.flag_richardson = False
     amrdata.flag2refine = True
 
     # steps to take on each level L between regriddings of level L+1:
     amrdata.regrid_interval = 1
 
-    # width of buffer zone around flagged points:
-    # (typically the same as regrid_interval so waves don't escape):
-    amrdata.regrid_buffer_width  = 1
+    # width of buffer zone around flagged points
+    amrdata.regrid_buffer_width = 1
 
     # clustering alg. cutoff for (# flagged pts) / (total # of cells refined)
-    # (closer to 1.0 => more small grids may be needed to cover flagged cells)
     amrdata.clustering_cutoff = 0.80000
 
     # print info about each regridding up to this level:
     amrdata.verbosity_regrid = 0
-
 
     #  ----- For developers -----
     # Toggle debugging print statements:
@@ -328,34 +214,22 @@ def setamr(rundata):
     amrdata.tprint = False      # time step reporting each level
     amrdata.uprint = False      # update/upbnd reporting
 
-    # More AMR parameters can be set -- see the defaults in pyclaw/data.py
+    # customize regions of refinement
+    # rundata.regiondata.regions.append([minlevel, maxlevel, t1, t2, x1, x2, y1, y2])
 
-    # == setregions.data values ==
-    regions = rundata.regiondata.regions
-    # to specify regions of refinement append lines of the form
-    #  [minlevel,maxlevel,t1,t2,x1,x2,y1,y2]
-
-    # == setgauges.data values ==
-    # for gauges append lines of the form  [gaugeno, x, y, t1, t2]
-    # rundata.gaugedata.gauges.append([])
+    # gauges
+    # rundata.gaugedata.gauges.append([gaugeno, x, y, t1, t2])
 
     return rundata
 
 
-#-------------------
-def setgeo(rundata):
-#-------------------
-    """
-    Set GeoClaw specific runtime parameters.
-    For documentation see ....
-    """
+def setgeo(rundata: data.ClawRunData):
+    """Set GeoClaw specific runtime parameters."""
 
     try:
         geo_data = rundata.geo_data
-    except:
-        print("*** Error, this rundata has no geo_data attribute")
-        raise AttributeError("Missing geo_data attribute")
-
+    except AttributeError as err:
+        raise AttributeError("This rundata has no geo_data attribute") from err
 
     # == Physics ==
     geo_data.gravity = 9.81
@@ -372,43 +246,28 @@ def setgeo(rundata):
     geo_data.manning_coefficient = 0.035
     geo_data.friction_depth = 1.e6
 
-    # extra parameters
-    geo_data.update_tol = geo_data.dry_tolerance
-    geo_data.refine_tol = 0.0
-
     # Refinement data
     refinement_data = rundata.refinement_data
     refinement_data.wave_tolerance = 1.e-5
     refinement_data.speed_tolerance = [1e-8]
-    refinement_data.deep_depth = 1e2
-    refinement_data.max_level_deep = 3
     refinement_data.variable_dt_refinement_ratios = True
 
-    # == settopo.data values ==
+    # for topography, append lines of the form [topotype, fname]
     topo_data = rundata.topo_data
-    # for topography, append lines of the form
-    #    [topotype, minlevel, maxlevel, t1, t2, fname]
-    topo_data.topofiles.append([3, 1, 5, 0., 1.e10, '../common-files/salt_lake_1.asc'])
+    topo_data.topofiles.append([3, '../common-files/salt_lake_1.asc'])
 
-    # == setdtopo.data values ==
-    dtopo_data = rundata.dtopo_data
-    # for moving topography, append lines of the form :   (<= 1 allowed for now!)
-    #   [topotype, minlevel,maxlevel,fname]
+    # for moving topography, append lines of the form: [topotype, fname]
+    # dtopo_data = rundata.dtopo_data
 
-    # == setqinit.data values ==
+    # for qinit perturbations, append lines of the form: [fname]
     rundata.qinit_data.qinit_type = 0
     rundata.qinit_data.qinitfiles = []
-    # for qinit perturbations, append lines of the form: (<= 1 allowed for now!)
-    #   [minlev, maxlev, fname]
 
-    # == setfixedgrids.data values ==
-    fixedgrids = rundata.fixed_grid_data
     # for fixed grids append lines of the form
-    # [t1,t2,noutput,x1,x2,y1,y2,xpoints,ypoints,\
-    #  ioutarrivaltimes,ioutsurfacemax]
+    # [t1, t2, noutput, x1, x2, y1, y2, xpoints, ypoints, ioutarrivaltimes, ioutsurfacemax]
+    # fixedgrids = rundata.fixed_grid_data
 
     # Land-spill module settings
-    from clawpack.geoclaw.data import LandSpillData
     rundata.add_data(LandSpillData(), 'landspill_data')
     landspill = rundata.landspill_data
     landspill.ref_mu = 0.6512 # cP @ 15 degree C
@@ -416,12 +275,15 @@ def setgeo(rundata):
     landspill.ambient_temperature = 25.
     landspill.density = 800. # kg / m^3 @ 15 degree C; will overwrite rho in GeoClaw
 
+    # extra parameters
+    landspill.update_tol = geo_data.dry_tolerance
+    landspill.refine_tol = 0.0
+
     # Point sources
     ptsources_data = landspill.point_sources
     ptsources_data.n_point_sources = 1
     ptsources_data.point_sources.append(
         [[-12459650., 4986000.], 2, [1800., 12600.], [0.5, 0.1]])
-
 
     # Darcy-Weisbach friction
     darcy_weisbach_data = landspill.darcy_weisbach_friction
@@ -441,14 +303,3 @@ def setgeo(rundata):
     evaporation_data.coefficients = [13.2, 0.21]
 
     return rundata
-    # end of function setgeo
-    # ----------------------
-
-
-
-if __name__ == '__main__':
-    # Set up run-time parameters and write all data files.
-    import sys
-    rundata = setrun(*sys.argv[1:])
-    rundata.write()
-
